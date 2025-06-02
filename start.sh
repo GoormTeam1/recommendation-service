f5a5aaea74aff07f26c71fd168def28715bf99f7
minseong1 <alstjd2627@naver.com>
2025-06-02 16:29:53 +0900

    log: 콘솔로그 복원

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 APP_NAME="recommendation-service"
 JAR_NAME="recommendation.jar"
 APP_DIR="/home/ubuntu/backend/$APP_NAME"
-LOG_FILE="$APP_DIR/$APP_NAME.log"
+LOG_FILE="$LOG_DIR/$APP_NAME.log"
 LOG_DIR="/home/ubuntu/backend/logs/service/$APP_NAME"
 ENV_FILE="$APP_DIR/../.env"
 
@@ -34,6 +34,6 @@ fi
 
 # ✅ 새 jar 실행
 echo "▶ 새 앱 실행 중..."
-nohup java -jar "$LOG_DIR" > "$LOG_FILE" 2>&1 &
+nohup java -jar "$APP_DIR/$JAR_NAME" > "$LOG_FILE" 2>&1 &
 
 echo "✅ [$APP_NAME] 배포 완료. 로그: $LOG_FILE"
